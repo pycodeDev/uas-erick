a844ad387a9f39cea794f531ae9eee40e147ccb9
M Anang R <44345333+pycodeDev@users.noreply.github.com>
2025-01-18 22:39:08 +0700

    Update db.php

--- process/db.php
+++ process/db.php
@@ -4,12 +4,19 @@ define('BASE_URL', 'http://uas-erick.test/');
 $host = 'localhost';
 $dbname = 'uas_db';
 $username = 'root';
-$password = '';
+$password = '12345678';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
+}
+
+function base_url($path = '') {
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+    $host = $_SERVER['HTTP_HOST'];
+    $base = $protocol . "://" . $host . dirname($_SERVER['SCRIPT_NAME']);
+    return rtrim($base, '/') . '/' . ltrim($path, '/');
 }
 ?>
